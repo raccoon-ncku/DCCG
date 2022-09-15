8ca54231ed9bae5563a4bcdc92c89f6cc811fbd6
ChiaChing Yen <ccyen@umich.edu>
2022-09-15 12:46:52 +0800

    add input

--- Lecture/Lecture_01/Examples/3.1.2_data_type.py
+++ Lecture/Lecture_01/Examples/3.1.2_data_type.py
@@ -9,6 +9,13 @@ c = 34.0
 print("c is ", type(c))
 
 d = "68"
-print(a+d, type(a+d))
-print(b+c, type(b+c))
-print(a+b)
+print("a+d is ", a+d, type(a+d))
+print("b+c is ", b+c, type(b+c))
+# print("a+b is ", a+b, type(a+b))
+
+'''
+e = input("Please in put a number:")
+print("input e is ", e, type(e))
+e = int(e)
+print("after int(), e becomes ", e, type(e))
+'''
