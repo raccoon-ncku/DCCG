c51d771d8f49becf2514ae2cf83795d253fb1e67
ChiaChing Yen <ccyen@umich.edu>
2023-10-19 09:17:09 +0800

    remove answer

--- Exercise/Lecture_02/is_palindrome.py
+++ Exercise/Lecture_02/is_palindrome.py
@@ -7,7 +7,4 @@ def is_palindrome(string):
     # convert to lower case
     string = string.lower()
     # Check if string is a palindrome
-    for i in range(len(string) // 2):
-        if string[i] != string[-i - 1]:
-            return False
-    return True
+    pass
