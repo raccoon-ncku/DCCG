123e95df0ce73e400a198b74ae0ac721e3744f43
ChiaChing-Yen <ccyen@umich.edu>
2023-11-09 01:16:55 +0800

    update to week 9

--- Lecture/Lecture_04/python_examples/7.2.2_path_join_I.py
+++ Lecture/Lecture_04/python_examples/7.2.2_path_join_I.py
@@ -14,10 +14,17 @@ dict = {
     "year": 1964
 }
 
+# use os.path.join() to join the path
+# This will work on any operating system
+# E.g., Windows, Mac, Linux
 output_path = os.path.join(
     os.path.dirname(__file__),
     'result.json'
 )
+
+# if not using os.path.join(), the path needs to be coded manually
+# Windows: output_path = os.path.dirname(__file__) + '\\result.json'
+# macOS/Linux Distributions: output_path = os.path.dirname(__file__) + '/result.json'
 
 with open(output_path, 'w') as fp:
     # save the dictionary to the json file
