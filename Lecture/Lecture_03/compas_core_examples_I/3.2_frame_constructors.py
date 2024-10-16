4e3fde19d9c56e24333b4bebda46b739cecd3a16
Chia-Ching Yen <ccyen@umich.edu>
2024-10-16 22:58:04 +0800

    chore: refine example structure

--- Lecture/Lecture_03/compas_core_examples_I/3.2_frame_constructors.py
+++ Lecture/Lecture_03/compas_core_examples_I/3.2_frame_constructors.py
@@ -1,15 +1,20 @@
 import compas.geometry as cg
 
 # Frame autocorrects axes to be orthonormal
-F = cg.Frame(cg.Point(1, 0, 0), cg.Vector(-0.45, 0.1, 0.3), cg.Vector(1, 0, 0))
+f1 = cg.Frame(cg.Point(1, 0, 0), cg.Vector(-0.45, 0.1, 0.3), cg.Vector(1, 0, 0))
 
 # Frames can also be constructed from simple lists or tuples containing
 # the XYZ coordinates of the base point and the two input vectors.
-F = cg.Frame([1, 0, 0], [-0.45, 0.1, 0.3], [1, 0, 0])
+f2 = cg.Frame([1, 0, 0], [-0.45, 0.1, 0.3], [1, 0, 0])
 
 # Frames can also be constructed from three points.
-F = cg.Frame.from_points([1, 1, 1], [2, 3, 6], [6, 3, 0])
+f3 = cg.Frame.from_points([1, 1, 1], [2, 3, 6], [6, 3, 0])
 
 # Frames can also be constructed to
 # match specific planes of the world coordinate system.
-F = cg.Frame.worldXY()
+f4 = cg.Frame.worldXY()
+
+print("f1", f1)
+print("f2", f2)
+print("f3", f3)
+print("f4", f4)
