ed683931b37414dc7f25d0f3c819f0a9dacfd9a3
Chia-Ching Yen <ccyen@umich.edu>
2024-10-16 18:15:08 +0800

    Refactor prime benchmark and prime numbers modules

--- Lecture/Lecture_03/compas_core_examples_I/5.1_shapes.py
+++ Lecture/Lecture_03/compas_core_examples_I/5.1_shapes.py
@@ -1,21 +1,19 @@
 import compas.geometry as cg
-from compas_view2.app import App
+from compas_viewer import Viewer
 
 # Box is created from a frame and xsize, ysize, zsize
-box = cg.Box(cg.Frame.worldXY(), 10, 1, 4)
+box = cg.Box(1, 2, 4, cg.Frame.worldXY())
 
 # Sphere is created from a point and radius
-sphere = cg.Sphere([10, 0, 0], 4)
+sphere = cg.Sphere(1, cg.Frame([5, 0, 0], [-0.45, 0.1, 0.3], [1, 0, 0]))
 
 # Cylinder is created from a circle and height
-plane = cg.Plane([20, 0, 0], [0, 0, 1])
-circle = cg.Circle(plane, 5)
-cylinder = cg.Cylinder(circle, height=4)
+cylinder = cg.Cylinder(0.5, 0.6, cg.Frame([10, 0, 0], [-0.45, 0.1, 0.3], [1, 0, 0]))
 
 geometries = [box, sphere, cylinder]
 
-# visualize
-viewer = App()
+# Visualization
+viewer = Viewer()
 for geometry in geometries:
-    viewer.add(geometry)
-viewer.run()
+    viewer.scene.add(geometry)
+viewer.show()
